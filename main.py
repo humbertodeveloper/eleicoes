3ca1d5b0d9769bd60042b33b3582f52f0945115d
Humberto Aquino <humbertocgrj@gmail.com>
2022-10-02 20:07:42 -0300

    Permitindo atualizar em 5 segundos

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import requests
 import json
+import time
 import pandas as pd
 
-data = requests.get(
-    'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
 
-json_data = json.loads(data.content)
+while True:
 
-candidato = []
-votos = []
-porcentagem = []
+    try:
+        data = requests.get(
+            'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
 
-for informacoes in json_data['cand']:
+        json_data = json.loads(data.content)
 
-    if int(informacoes['seq']) in range(1, 12):
-        candidato.append(informacoes['nm'])
-        votos.append(informacoes['vap'])
-        porcentagem.append(f"{informacoes['pvap']}%")
+        candidato = []
+        votos = []
+        porcentagem = []
 
-df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
-    'Candidato', 'Nº de Votos', 'Porcentagem'
-])
+        for informacoes in json_data['cand']:
 
-print(f"Urnas apuradas: {json_data['psi']}% \n")
-print(f"{df_eleicao} \n")
-print(f"Última atualização: {json_data['dt']} {json_data['ht']}")
+            if int(informacoes['seq']) in range(1, 12):
+                candidato.append(informacoes['nm'])
+                votos.append(informacoes['vap'])
+                porcentagem.append(f"{informacoes['pvap']}%")
+
+        df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
+            'Candidato', 'Nº de Votos', 'Porcentagem'
+        ])
+
+        print(f"\nUrnas apuradas: {json_data['psi']}% \n")
+        print(f"{df_eleicao} \n")
+        print(f"Última atualização: {json_data['dt']} {json_data['ht']}")
+    except:
+        print('Atualizando novamente...')
+
+    time.sleep(5)
