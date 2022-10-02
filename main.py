2c7afba1246e52bc6946e9cab8628c73f348a371
Humberto Aquino <humbertocgrj@gmail.com>
2022-10-02 19:34:22 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import pandas as pd
+
+data = requests.get(
+    'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
+
+json_data = json.loads(data.content)
+
+candidato = []
+votos = []
+porcentagem = []
+
+for informacoes in json_data['cand']:
+
+    if int(informacoes['seq']) in range(1, 12):
+        candidato.append(informacoes['nm'])
+        votos.append(informacoes['vap'])
+        porcentagem.append(f"{informacoes['pvap']}%")
+
+df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns=[
+    'Candidato', 'Nº de Votos', 'Porcentagem'
+])
+
+print(f"Urnas apuradas: {json_data['psi']}% \n")
+print(f"{df_eleicao} \n")
+print(f"Última atualização: {json_data['dt']} {json_data['ht']}")
